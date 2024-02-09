73c40e68e2b36a8681ab40e5418296f859d7be96
jermito24 <jermito24@gmail.com>
2024-02-09 19:13:00 +0530

    added camera, lidar, base gazebo farming world

--- launch/sim.launch.py
+++ launch/sim.launch.py
@@ -13,15 +13,20 @@ def generate_launch_description():
 
     package_name='agri_bot' 
 
+    world = os.path.join(get_package_share_directory(package_name), 'world', 'agri_world.gazebo')
+
     rsp = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
                     get_package_share_directory(package_name),'launch','rsp.launch.py'
                 )]),
     )
 
+    gazebo_params_file = os.path.join(get_package_share_directory(package_name),'config','gazebo_params.yaml')
+
     gazebo = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
                     get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
+                    launch_arguments={'verbose' : 'true', 'world': world, 'extra_gazebo_args': '--ros-args --params-file ' + gazebo_params_file}.items()
              )
 
     # Run the spawner node from the gazebo_ros package. The entity name doesn't really matter if you only have a single robot.
